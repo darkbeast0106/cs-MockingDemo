aa4948a51e7f516573d6ce457821fcb8f6ab7e4c
Budaházi Bence <budahazi.bence@petrik.hu>
2024-04-23 22:14:18 +0200

    fix: Fix filename typo

--- MockingDemo/Program.cs
+++ MockingDemo/Program.cs
@@ -1,6 +1,6 @@
 ﻿using MockingDemo;
 
-PersonService service = new PersonService("perople.json");
+PersonService service = new PersonService("people.json");
 PersonStatistics statistics = new PersonStatistics(service);
 
 Console.WriteLine("Átlag életkor: {0}", statistics.GetAverageAge());
